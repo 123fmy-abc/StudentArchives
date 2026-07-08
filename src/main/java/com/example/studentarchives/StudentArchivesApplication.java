69b4687e3ce27e02a648155676af4e1a751bde69
123fmy-abc <3227605507@qq.com>
2026-07-08 17:07:53 +0800

    框架

---
+++ src/main/java/com/example/studentarchives/StudentArchivesApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentarchives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentArchivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentArchivesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentarchives/StudentArchivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentarchives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentArchivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
