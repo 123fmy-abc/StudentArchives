7cf99fa6d91daed7f2329cf81c7e29b3b7624a1d
Lizhiwei415221 <193952040@qq.com>
2026-08-04 17:39:01 +0800

    Create ActivityStatusResponse.java

---
+++ src/main/java/com/example/studentarchives/dto/Fmy/activity/response/ActivityStatusResponse.java
@@ -0,0 +1,30 @@
+package com.example.studentarchives.dto.Fmy.activity.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 动态记录状态变更响应（编辑/删除/撤回后的统一返回体）
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityStatusResponse {
+
+    private Long id;
+    private String type;
+    private Integer status;
+
+    @JsonProperty("status_label")
+    private String statusLabel;
+
+    @JsonProperty("current_version")
+    private Integer currentVersion;
+
+    @JsonProperty("submit_count")
+    private Integer submitCount;
+}
