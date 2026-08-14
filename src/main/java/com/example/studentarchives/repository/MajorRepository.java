47f1a5cea447c182df42aebf347dd1bdc32ff424
Lizhiwei415221 <193952040@qq.com>
2026-08-14 21:18:30 +0800

    Update MajorRepository.java

--- src/main/java/com/example/studentarchives/repository/MajorRepository.java
+++ src/main/java/com/example/studentarchives/repository/MajorRepository.java
@@ -4,9 +4,14 @@ import com.example.studentarchives.entity.org.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 /**
  * 专业 Repository
  */
 @Repository
 public interface MajorRepository extends JpaRepository<Major, Long> {
+
+    List<Major> findByIdIn(Collection<Long> ids);
 }
