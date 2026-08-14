40f9f2d0d422183140b11ec2b4fd8fed4b78c517
Lizhiwei415221 <193952040@qq.com>
2026-08-14 21:18:48 +0800

    Update UserRoleRepository.java

--- src/main/java/com/example/studentarchives/repository/UserRoleRepository.java
+++ src/main/java/com/example/studentarchives/repository/UserRoleRepository.java
@@ -4,6 +4,7 @@ import com.example.studentarchives.entity.user.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -13,4 +14,8 @@ import java.util.List;
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
     List<UserRole> findByUserId(Long userId);
+
+    List<UserRole> findByUserIdIn(Collection<Long> userIds);
+
+    List<UserRole> findByRoleId(Long roleId);
 }
