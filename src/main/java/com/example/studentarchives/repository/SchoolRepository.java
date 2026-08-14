4f656da52a4ddefcece9dacc947719c3f7fd1161
Lizhiwei415221 <193952040@qq.com>
2026-08-14 21:18:32 +0800

    Update SchoolRepository.java

--- src/main/java/com/example/studentarchives/repository/SchoolRepository.java
+++ src/main/java/com/example/studentarchives/repository/SchoolRepository.java
@@ -4,9 +4,14 @@ import com.example.studentarchives.entity.org.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 /**
  * 学校 Repository
  */
 @Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
+
+    List<School> findByIdIn(Collection<Long> ids);
 }
