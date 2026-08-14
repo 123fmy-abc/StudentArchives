e198901f85397e44b05d543e67fea48c6afc318c
Lizhiwei415221 <193952040@qq.com>
2026-08-14 21:18:00 +0800

    Update ClazzRepository.java

--- src/main/java/com/example/studentarchives/repository/ClazzRepository.java
+++ src/main/java/com/example/studentarchives/repository/ClazzRepository.java
@@ -4,9 +4,16 @@ import com.example.studentarchives.entity.org.Clazz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 /**
  * 班级 Repository
  */
 @Repository
 public interface ClazzRepository extends JpaRepository<Clazz, Long> {
+
+    List<Clazz> findByGrade(String grade);
+
+    List<Clazz> findByIdIn(Collection<Long> ids);
 }
