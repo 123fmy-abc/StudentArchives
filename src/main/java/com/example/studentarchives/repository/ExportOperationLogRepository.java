e7ddb3b54c13e700dcc31b768c0388c20bd3ec3d
123fmy-abc <3227605507@qq.com>
2026-08-16 15:24:29 +0800

    Update ExportOperationLogRepository.java

--- src/main/java/com/example/studentarchives/repository/ExportOperationLogRepository.java
+++ src/main/java/com/example/studentarchives/repository/ExportOperationLogRepository.java
@@ -5,10 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * 导出操作日志 Repository（对应表 export_operation_logs）
  */
 @Repository
 public interface ExportOperationLogRepository extends JpaRepository<ExportOperationLog, Long>,
         JpaSpecificationExecutor<ExportOperationLog> {
+
+    /**
+     * 查询某个文件最近一次指定操作的日志（下载审计以创建记录为模板）
+     *
+     * @param fileId 文件 ID
+     * @param action 操作类型（1=创建，2=下载）
+     */
+    Optional<ExportOperationLog> findTopByFileIdAndActionOrderByCreatedAtDesc(Long fileId, Integer action);
 }
