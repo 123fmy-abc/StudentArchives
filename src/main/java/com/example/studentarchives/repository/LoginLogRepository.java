0635f1f3cd2dcf950c74a3ca5ac51bde4e8b8597
Lizhiwei415221 <193952040@qq.com>
2026-08-16 12:07:55 +0800

    Update LoginLogRepository.java

--- src/main/java/com/example/studentarchives/repository/LoginLogRepository.java
+++ src/main/java/com/example/studentarchives/repository/LoginLogRepository.java
@@ -2,6 +2,7 @@ package com.example.studentarchives.repository;
 
 import com.example.studentarchives.entity.log.LoginLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +11,7 @@ import java.util.Optional;
  * 登录日志 Repository
  */
 @Repository
-public interface LoginLogRepository extends JpaRepository<LoginLog, Long> {
+public interface LoginLogRepository extends JpaRepository<LoginLog, Long>, JpaSpecificationExecutor<LoginLog> {
 
     Optional<LoginLog> findFirstByUserIdOrderByCreatedAtDesc(Long userId);
 }
