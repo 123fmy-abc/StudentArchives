a5d236bb46b7d4b5ea862ce5de8ecea811540e4a
Lizhiwei415221 <193952040@qq.com>
2026-08-16 12:07:58 +0800

    Update ExportOperationLogRepository.java

--- src/main/java/com/example/studentarchives/repository/ExportOperationLogRepository.java
+++ src/main/java/com/example/studentarchives/repository/ExportOperationLogRepository.java
@@ -2,11 +2,13 @@ package com.example.studentarchives.repository;
 
 import com.example.studentarchives.entity.export.ExportOperationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
  * 导出操作日志 Repository（对应表 export_operation_logs）
  */
 @Repository
-public interface ExportOperationLogRepository extends JpaRepository<ExportOperationLog, Long> {
+public interface ExportOperationLogRepository extends JpaRepository<ExportOperationLog, Long>,
+        JpaSpecificationExecutor<ExportOperationLog> {
 }
